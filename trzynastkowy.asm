ea473751a162722943e3823fda2dc92778025461
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2023-11-11 22:38:01 +0100

    Update and rename cos32.asm to trzynastkowy.asm

--- trzynastkowy.asm
+++ trzynastkowy.asm
@@ -6,14 +6,14 @@ extern _ExitProcess@4 : PROC
 
 public _main
 .data
-; wczytywanie liczby dziesiêtnej z klawiatury – po
-; wprowadzeniu cyfr nale¿y nacisn¹æ klawisz Enter
-; liczba po konwersji na postaæ binarn¹ zostaje wpisana
+; wczytywanie liczby dziesiÄ™tnej z klawiatury â€“ po
+; wprowadzeniu cyfr naleÅ¼y nacisnÄ…Ä‡ klawisz Enter
+; liczba po konwersji na postaÄ‡ binarnÄ… zostaje wpisana
 ; do rejestru EAX
 ; deklaracja tablicy do przechowywania wprowadzanych cyfr
 ; (w obszarze danych)
 obszar db 12 dup (?)
-dziesiec dd 13			; mno¿nik
+dziesiec dd 13			; mnoÅ¼nik
 znaki db 12 dup (?)
 flaga dd 0 
 dekoder db '0123456789ABC'
@@ -23,7 +23,7 @@ dekoder db '0123456789ABC'
 wyswietl_EAX PROC
 
 mov esi, 10 ; indeks w tablicy 'znaki'
-mov ebx, 10 ; dzielnik równy 10
+mov ebx, 10 ; dzielnik rÃ³wny 10
 ;mov eax, 5804
 mov byte PTR znaki [1], '+'
 cmp eax, 0
@@ -35,7 +35,7 @@ neg eax
 mov byte PTR znaki [1], '-'
 
 konwersja:
-mov edx, 0 ; zerowanie starszej czêœci dzielnej
+mov edx, 0 ; zerowanie starszej czÄ™Å›ci dzielnej
 div ebx ; dzielenie przez 10, reszta w EDX,
 ; iloraz w EAX
 cmp dl, 0AH
@@ -52,8 +52,8 @@ mov znaki[esi], dl; zapisanie cyfry w kodzie ASCII
 dec esi ; zmniejszenie indeksu
 cmp eax, 0 ; sprawdzenie czy iloraz = 0
 jne konwersja ; skok, gdy iloraz niezerowy
-; wype³nienie pozosta³ych bajtów spacjami i wpisanie
-; znaków nowego wiersza
+; wypeÅ‚nienie pozostaÅ‚ych bajtÃ³w spacjami i wpisanie
+; znakÃ³w nowego wiersza
 
 wypeln:
 cmp esi, 1
@@ -65,13 +65,13 @@ jmp wypeln
 wyswietl:
 mov byte PTR znaki [0], 0AH ; kod nowego wiersza
 mov byte PTR znaki [11], 0AH ; kod nowego wiersza
-; wyœwietlenie cyfr na ekranie
-push dword PTR 12 ; liczba wyœwietlanych znaków
-push dword PTR OFFSET znaki ; adres wyœw. obszaru
-push dword PTR 1; numer urz¹dzenia (ekran ma numer 1)
-call __write ; wyœwietlenie liczby na ekranie
+; wyÅ›wietlenie cyfr na ekranie
+push dword PTR 12 ; liczba wyÅ›wietlanych znakÃ³w
+push dword PTR OFFSET znaki ; adres wyÅ›w. obszaru
+push dword PTR 1; numer urzÄ…dzenia (ekran ma numer 1)
+call __write ; wyÅ›wietlenie liczby na ekranie
 
-add esp, 12 ; usuniêcie parametrów ze stosu
+add esp, 12 ; usuniÄ™cie parametrÃ³w ze stosu
 ret
 wyswietl_EAX ENDP
 
